0a179926c072fd72a1148dfb73fe1baace81bd78
henrykerron <henrykerron@gmail.com>
2014-12-09 12:13:49 -0500

    migrated topics-table

---
+++ db/migrate/20141209171102_create_topics.rb
@@ -0,0 +1,10 @@
+class CreateTopics < ActiveRecord::Migration
+  def change
+  	create_table :topics do |t|
+  		t.string :topic, null: false
+  		t.integer :meetup_id, null: false
+
+  		t.timestamps
+  	end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209170507) do
+ActiveRecord::Schema.define(version: 20141209171102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20141209170507) do
 
   create_table "rsvps", force: true do |t|
     t.integer  "user_id",    null: false
+    t.integer  "meetup_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "topics", force: true do |t|
+    t.string   "topic",      null: false
     t.integer  "meetup_id",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
