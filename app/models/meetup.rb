0e650ce9ec54469f9903e445306a66e759b3b75a
henrykerron <henrykerron@gmail.com>
2014-12-11 12:04:23 -0500

    Added comments-table

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,4 +1,5 @@
 class Meetup <ActiveRecord::Base
 	has_many :rsvps
 	has_many :users, through: :rsvps 
+	has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 has_many :rsvps
 has_many :meetps, through: :rspvs
+has_many :comments
 
   def self.find_or_create_from_omniauth(auth)
     provider = auth.provider
